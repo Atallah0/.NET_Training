428c96c8cf92a0c669a312994ee31ac9bed93cbc
Atallah <Atallah.192000@gmail.com>
2024-02-09 04:58:33 +0200

    Create-Update-Delete API Added to Project

--- Application/Project/Controllers/ProjectsController.cs
+++ Application/Project/Controllers/ProjectsController.cs
@@ -3,6 +3,7 @@ using Infrastructure.DBContext;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 // using Domain.Project.Models;
+
 namespace Application.Project.Controllers
 {
     [ApiController]
@@ -20,7 +21,6 @@ namespace Application.Project.Controllers
         public async Task<ActionResult<List<Domain.Project.Models.Project>>> GetProjects()
         {
             var projects = await _repo.GetProjectsAsync();
-
             return Ok(projects);
         }
 
@@ -28,6 +28,32 @@ namespace Application.Project.Controllers
         public async Task<ActionResult<Domain.Project.Models.Project>> GetProject(int id)
         {
             return await _repo.GetProjectByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Domain.Project.Models.Project>> CreateProject([FromBody] Domain.Project.Models.Project project)
+        {
+            var createdProject = await _repo.CreateProjectAsync(project);
+            return CreatedAtAction(nameof(GetProject), new { id = createdProject.Id }, createdProject);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Domain.Project.Models.Project>> UpdateProject(int id, [FromBody] Domain.Project.Models.Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest("Id in URL and body doesn't match");
+            }
+            
+            var updatedProject = await _repo.UpdateProjectAsync(project);
+            return Ok(updatedProject);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            await _repo.DeleteProjectAsync(id);
+            return NoContent();
         }
     }
 }
